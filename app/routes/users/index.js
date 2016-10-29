aa9da7e47bc1e85e3701f8a68c38d0069c6c607b
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2016-10-29 18:00:33 -0400

    fixing

--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -28,7 +28,7 @@ router.post('/favorite', (req, res, next) => {
         .then(user => {
             if (user.favorites.indexOf(req.body.projectId) == -1) {
                 let prev = user.favorites;
-                user.favorites = prev.push(re.qoby.projectId);
+                user.favorites = prev.push(req.boby.projectId);
                 return user.save();
             }
         })
